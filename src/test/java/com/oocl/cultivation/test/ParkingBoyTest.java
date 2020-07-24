49d6c731ef20a750789dc14a3ae3778cce70670a
CHENRO6 <chenro6@oocl.com>
2020-07-24 20:20:44 +0800

    [TDD Parking Lot][Roy] test: should_return_2_ticket_when_parking_car_given_2_car_and_1_packing_boy

--- src/test/java/com/oocl/cultivation/test/ParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/test/ParkingBoyTest.java
@@ -6,6 +6,8 @@ import com.oocl.cultivation.entity.Ticket;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 public class ParkingBoyTest {
 
     @Test
@@ -28,6 +30,20 @@ public class ParkingBoyTest {
         Car car = parkingBoy.fetchingCar(ticket);
         // then
         Assertions.assertEquals("CAR001", car.getId());
+    }
+
+    @Test
+    void should_return_2_ticket_when_parking_car_given_2_car_and_1_packing_boy() {
+        // given
+        ParkingBoy parkingBoy = new ParkingBoy();
+        Car car001 = new Car("CAR001");
+        Car car002 = new Car("CAR002");
+        // when
+        Ticket ticket1 = parkingBoy.parkingCar(car001);
+        Ticket ticket2 = parkingBoy.parkingCar(car002);
+        // then
+        Assertions.assertEquals("CAR001", ticket1.getNumber());
+        Assertions.assertEquals("CAR002", ticket2.getNumber());
     }
 
 }
